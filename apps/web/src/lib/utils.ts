22e34baa6bb4e47e1a8aa1f7f5df0fad2507655c
Ayush tomar <ayushtomar388@gmail.com>
2025-01-16 23:49:26 +0530

    Tailwind merge utils created

---
+++ apps/web/src/lib/utils.ts
@@ -0,0 +1,7 @@
+import * as clsx from "clsx";
+import type { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]): string {
+    return twMerge(clsx.default(inputs));
+}
